87dd714b24ddc3f5c938d18255c10e22b5da6567
Yorick <2524964538@qq.com>
2026-05-26 23:43:39 +0800

    feat: add clipboard permission guide for English and Chinese FAQs

--- app/en/faq/_meta.js
+++ app/en/faq/_meta.js
@@ -1,5 +1,6 @@
 export default {
   'compat-mode': 'Compatibility Mode',
   'hard-line-breaks': 'Hard Line Breaks',
-  'disable-auto-numbering': 'Disable Auto Numbering'
+  'disable-auto-numbering': 'Disable Auto Numbering',
+  'clipboard-permission': 'How to Enable Clipboard Permission'
 }
--- app/faq/_meta.js
+++ app/faq/_meta.js
@@ -1,5 +1,6 @@
 export default {
   'compat-mode': '兼容模式',
   'hard-line-breaks': '强制换行',
-  'disable-auto-numbering': '禁用自动编号'
+  'disable-auto-numbering': '禁用自动编号',
+  'clipboard-permission': '如何打开剪贴板权限'
 }
